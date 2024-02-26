899eb10b62d271fabdc2bc004a7ae59c659f6745
home <nuttapong_coach@hotmail.com>
2024-02-26 11:35:54 +0700

    add crud with dialog box

---
+++ Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace QuickGrid
+{
+	public class Product
+	{
+		public int Id { get; set; }
+		[Required]
+
+		public string? Name { get; set; }
+		[Required]
+
+		public decimal? Amount { get; set; }
+	}
+}
+
--- obj/Debug/net8.0/QuickGrid.AssemblyInfo.cs
+++ obj/Debug/net8.0/QuickGrid.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuickGrid")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6b08ed9e67df5e1f32a1bec755c0645987c05ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuickGrid")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuickGrid")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
